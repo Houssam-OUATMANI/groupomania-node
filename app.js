fc81824eaaa261b76425fdf7898b9090bd79e81d
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2021-02-19 00:32:26 +0100

    Mise en place du ctrl user

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express') 
 const cors = require('cors')
-const dotenv = require('dotenv').config()
+require('dotenv').config()
+const { PORT } = process.env
 
 const app = express()
+
+const userRoutes = require('./routes/user.routes')
+const database = require('./config/database')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('./public'))
 
 
+app.use('/api/auth',userRoutes)
 
-const { PORT } = process.env
-app.listen(PORT || 5000 , () => PORT ? console.log(`PORT SERVER ${PORT}`) : console.log("SERVER PORT 5000"))
+
+
+
+database.sync()
+.then(()=>{
+    console.log('Database reached ...')
+    app.listen(PORT || 5000 , () => PORT ? console.log(`PORT SERVER ${PORT}`) : console.log("SERVER PORT 5000"))
+})
+.catch(err => console.log(err))
